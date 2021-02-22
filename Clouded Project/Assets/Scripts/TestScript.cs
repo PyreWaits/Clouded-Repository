29de7755b4b1f5fb90ac529b90b509b01e568497
PyreWaits <78356601+PyreWaits@users.noreply.github.com>
2021-02-22 12:53:00 +0000

    Test Script for learning basics

---
+++ Clouded Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Variables that will be used in Unity
+    public string startText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError(startText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning(startText);
+    }
+}
