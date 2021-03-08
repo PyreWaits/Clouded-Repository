b4005bf00ea1357ff214233c81e5a6864844a8fd
PyreWaits <78356601+PyreWaits@users.noreply.github.com>
2021-03-08 12:21:53 +0000

    Made the movement click to move

--- Clouded Project/Assets/Scripts/PlayerMovement.cs
+++ Clouded Project/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,90 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+
+// This script assumes the player will have a Rigidbody2D attached
+// and it won't work without it
+// This line enforces that the object have a Rigidbody2D in the 
+// Unity editor
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    // ------------------------------------------------
+    // Public variables, visible in Unity Inspector
+    // Use these for settings for your script
+    // that can be changed easily
+    // ------------------------------------------------
+
+    // This variable will hold the strength of the force
+    // propelling us toward our target
+    // (ie our speed)
+    public float forceStrength;
+    // This variable will determine how close we get to 
+    // the target point before stopping
+    public float stopDistance;
+
+    // ------------------------------------------------
+    // Private variables, NOT visible in the Inspector
+    // Use these for tracking data while the game
+    // is running
+    // ------------------------------------------------
+
+    // This variable will hold the last point we touched
+    // so we can keep moving to that point even after we
+    // stop touching the screen
+    private Vector2 targetPoint;
+
+    // This variable will store the attached RigidBody 
+    // so we can use it to move
+    Rigidbody2D ourRigidbody;
+
+
+    // ------------------------------------------------
+    // Awake is called when the script is loaded
+    // ------------------------------------------------
+    void Awake()
+    {
+        // Get the rigidbody that we'll be using for movement
+        ourRigidbody = GetComponent<Rigidbody2D>();
+
+        // Set our target point to be our current position
+        // so we don't start moving right away
+        targetPoint = transform.position;
+    }
+
+    // ------------------------------------------------
+    // Update is called once per frame
+    // ------------------------------------------------
+    void Update()
+    {
+        // If the mouse button is down or touch is detected...
+        if (Input.GetMouseButton(0))
+        {
+            // Record the mouse / touch position as our target
+            // Do this by using the camera to convert from screen 
+            // coordinates to game world coordinates
+            targetPoint = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        // How far away are we from the target?
+        float distance = (targetPoint - (Vector2)transform.position).magnitude;
+
+        // If we are farther away from our target than our minimum distance...
+        if (distance > stopDistance)
+        {
+            // ... Move in the direction of our target
+
+            // Get the direction
+            // Subtract the current position from the target position to get a distance vector
+            // Normalise changes it to be length 1, so we can then multiply it by our speed / force
+            Vector2 direction = (targetPoint - (Vector2)transform.position).normalized;
+
+            // Move in the correct direction with the set force strength
+            ourRigidbody.AddForce(direction * forceStrength);
+        }
+    }
+}
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
 
@@ -27,3 +110,4 @@ public class PlayerMovement : MonoBehaviour
     }
 
 }
+*/
